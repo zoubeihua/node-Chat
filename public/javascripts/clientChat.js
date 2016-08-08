ae52bda0936d015e74ada5d65430334b07404746
zoubeihua <240917020@qq.com>
2016-08-09 00:40:00 +0800

    添加了客户端和服务端之间通信逻辑

--- public/javascripts/clientChat.js
+++ public/javascripts/clientChat.js
@@ -2,3 +2,9 @@
  * Created by beihua on 2016/8/7.
  */
 var socket = io();
+// socket.on('online',function(msg){
+//     ShowMsg(1);
+// });
+// socket.on('offline',function(msg){
+//     ShowMsg(0);
+// })
