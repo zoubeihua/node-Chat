1f38a22f43656a4018adf7944f767ef3a1dfefec
zoubeihua <240917020@qq.com>
2016-08-09 00:39:15 +0800

    修改了服务端登录逻辑

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var io = require('socket.io')();
+var userList = [];
 io.on("connection",function (socket) {
-    console.log('用户上线')
-    socket.on("disconnect",function () {
-        console.log('用户离线')
+   //登录的方法
+    socket.on('login',function(user){
+        user.id = socket.id;
+        userList.push(user);
+        //向所有用户发送用户列表
+        io.emit('uesrList',userList);
+        //向用户本身发送信息
+        socket.emit('userInfo',user);
+        //除自己本身向所有用户发送消息
+        socket.broadcast.emit('loginInfo',user.name+"上线了。");
     })
 });
 exports.listen = function (_server) {
